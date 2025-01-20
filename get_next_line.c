f75a7c20443f133d183fabf934e780a9b4ff35bf
Denis Semenov <dsemenov@student.42lyon.fr>
2025-01-20 19:42:22 +0100

    Fix leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 17:57:33 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/01/20 14:04:37 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/01/20 19:36:15 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,97 +14,135 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-static char	*read_to_remainder(int fd, char *remainder)
+static char	*read_loop(int fd, char *remainder, char *buffer)
 {
-  char  *buffer;
-  ssize_t bytes_read;
+	ssize_t	bytes_read;
+	char	*tmp;
 
-  buffer = (char *) malloc((BUFFER_SIZE + 1) * (sizeof (char)));
-  if (!buffer)
-    return (NULL);
-  bytes_read = 1;
-  while (bytes_read > 0 && !ft_strchr(buffer, '\n'))
-  {
-    bytes_read = read(fd, buffer, BUFFER_SIZE);
-    if (bytes_read < 0)
+	bytes_read = 1;
+	while (bytes_read > 0 && !ft_strchr(remainder, '\n'))
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
     {
-      free(buffer);
+      free(remainder);
       return (NULL);
     }
-    buffer[bytes_read] = '\0';
-    remainder = ft_strjoin(remainder, buffer);
-    if (!remainder)
-      return (NULL);
-  }
-  free(buffer);
-  return (remainder);
+		buffer[bytes_read] = '\0';
+		tmp = ft_strjoin(remainder, buffer);
+		free(remainder);
+		if (!tmp)
+			return (NULL);
+		remainder = tmp;
+	}
+	return (remainder);
 }
 
-static char *extract_line(char *remainder)
+static char	*read_to_remainder(int fd, char *remainder)
 {
-  char  *line;
-  size_t i;
+	char	*buffer;
 
-  i = 0;
-  while (remainder[i] && remainder[i] != '\n')
-   i++; 
-  if (remainder[i] == '\n')
-    i++;
-  line = ft_substr(remainder, 0, i);
-  if (!line)
-    return (NULL);
-  return (line);
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+	{
+		free(remainder);
+		return (NULL);
+	}
+	remainder = read_loop(fd, remainder, buffer);
+	free(buffer);
+	if (!remainder)
+		return (NULL);
+	return (remainder);
 }
 
-static char *clean_remainder(char *remainder)
+
+static char	*extract_line(char *remainder)
 {
-  remainder = ft_strchr(remainder, '\n');
-  if (!remainder)
-    return (NULL);
-  remainder++;
-  return (remainder);
+	char	*line;
+	size_t	i;
+
+	if (!remainder)
+		return (NULL);
+	i = 0;
+	while (remainder[i] && remainder[i] != '\n')
+		i++;
+	if (remainder[i] == '\n')
+		i++;
+	line = ft_substr(remainder, 0, i);
+	if (!line)
+		return (NULL);
+	return (line);
+}
+
+static char	*clean_remainder(char *remainder)
+{
+	size_t	len;
+	char	*tmp;
+	char	*new_start;
+
+	tmp = ft_strchr(remainder, '\n');
+	if (!tmp)
+	{
+		free(remainder);
+		return (NULL);
+	}
+	tmp++;
+	len = ft_strlen(tmp);
+	new_start = ft_substr(tmp, 0, len);
+	if (!new_start)
+	{
+		free(remainder);
+		return (NULL);
+	}
+	free(remainder);
+	return (new_start);
 }
 
 char	*get_next_line(int fd)
 {
-	static char *remainder;
-  char  *line;
+	static char	*remainder;
+	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	remainder = read_to_remainder(fd, remainder);
-  if (!remainder)
-    return (NULL);
-  line = extract_line(remainder);
-  if (!line) 
-  {
-    free(line);
-    return (NULL);
-  }
-  if (!line[0])
-  {
-    free(line);
-    return (NULL);
-  }
-  remainder = clean_remainder(remainder);
-
+	if (!remainder)
+		return (NULL);
+	line = extract_line(remainder);
+	if (!line)
+	{
+		free(remainder);
+		remainder = NULL;
+		return (NULL);
+	}
+	if (!line[0])
+	{
+		free(line);
+    free(remainder);
+    remainder = NULL;
+		return (NULL);
+	}
+	remainder = clean_remainder(remainder);
 	return (line);
 }
 /*
-#include <stdio.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 int	main(void)
 {
 	int		fd;
-	char	*res = "1";
+	char	*res;
 
 	fd = open("./text", O_RDONLY);
-  while (res)
+  do
   {
     res = get_next_line(fd);
     printf("%s", res);
+    free(res);
   }
+  while (res);
+
   return (0);
 }
 */
